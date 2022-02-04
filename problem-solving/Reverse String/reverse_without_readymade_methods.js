1e6db401442003a99c7476909552405cad0ddd6e
Swapnil Bankar <swapnil.bankar@veritas.com>
2022-02-04 09:19:18 +0530

    Added reverse line code  without using javascript method, reverse line but do not reverse word

---
+++ problem-solving/Reverse String/reverse_without_readymade_methods.js
@@ -0,0 +1,29 @@
+function reverseLine(str) {
+    let arr = [];
+    let sample = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== ' ') {
+            sample = sample + str[i];
+        } else {
+            arr.push(sample);
+            sample = '';
+        }
+        if (!str[i + 1]) {
+            arr.push(sample);
+        }
+    }
+    let newArr = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        newArr.push(arr[i]);
+    }
+
+    let result = '';
+    for (let i = 0; i < newArr.length; i++) {
+        if (newArr[i] !== ' ') {
+            result = result + ' ' + newArr[i];
+        }
+    }
+}
+
+let str = 'I work in javascript language';
+reverseLine(str);
